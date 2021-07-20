7b6c50f88f739012cd37c7ef56435942fa8ac6d1
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-07-20 23:53:13 +0530

    Almost completed submit button

--- src/Components/CreateRoom.tsx
+++ src/Components/CreateRoom.tsx
@@ -1,8 +1,8 @@
 import { useRef, FC as FunctionalComponent, FormEvent } from "react";
 import {
-  AvatarActionBtns,
-  AvatarsWrapper,
-  Page,
+	AvatarActionBtns,
+	AvatarsWrapper,
+	Page,
 } from "../Styled-components/CreateRoom.styled";
 
 import io from "socket.io-client";
@@ -17,165 +17,163 @@ import parse from "html-react-parser";
 import { constants } from "../Constants";
 import { AiOutlineReload } from "react-icons/ai";
 import { HiOutlineArrowDown } from "react-icons/hi";
-import { Button } from "../Styled-components/Customize.style";
-
 const CreateRoom: FunctionalComponent = () => {
-  type maxAvatarType = {
-    isNew: boolean;
-    number: number;
-  };
+	type maxAvatarType = {
+		isNew: boolean;
+		number: number;
+	};
 
-  const NameRef = useRef<HTMLInputElement | null>()!;
-  const RoomRef = useRef<HTMLInputElement | null>()!;
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [avatars, setAvatars] = useState<string[]>([]);
-  const [maxAvatarIndex, setMaxAvatar] = useState<maxAvatarType>({
-    number: 42,
-    isNew: true,
-  });
-  const [loading, setLoading] = useState<boolean>(false);
-  const [currentAvatar, setCurrentAvatar] = useState<string>("");
-  useEffect(() => {
-    NameRef.current?.focus();
-  }, []);
-  function handleClose() {
-    setIsModalOpen(false);
-  }
-  function setAvatarIcon(avatar: string): void {
-    handleClose();
-    setCurrentAvatar(avatar);
-  }
-  useEffect(() => {
-    if (maxAvatarIndex.isNew) {
-      let acc = [];
-      while (acc.length !== maxAvatarIndex.number) {
-        acc.push(returnRandomAvatar());
-      }
-      setAvatars(acc);
-    } else {
-      let acc = [...avatars];
-      while (acc.length !== maxAvatarIndex.number) {
-        acc.push(returnRandomAvatar());
-      }
-      setAvatars(acc);
-    }
-    setLoading(false);
-    console.log(maxAvatarIndex);
-  }, [maxAvatarIndex]);
-  function handleSubmit(e: FormEvent): void {
-    e.preventDefault();
-  }
-  useEffect(() => {
-    if (avatars.length === 42) {
-      setCurrentAvatar(avatars[0]);
-    }
-  }, [avatars]);
-  return (
-    <Page>
-      <Form onSubmit={(e) => handleSubmit(e)}>
-        <div className="field">
-          <span>Name</span>
-          <br />
-          {/* @ts-ignore */}
-          <input type="text" ref={NameRef} spellCheck placeholder="Your Name" />
-        </div>
-        <div className="field">
-          <span>Room Name</span>
-          <br />
-          <input
-            type="text"
-            //@ts-ignore
-            ref={RoomRef}
-            spellCheck
-            placeholder="Name Your Room"
-          />
-        </div>
-        <div className="field">
-          <span>Avatar</span>
-          {currentAvatar && parse(currentAvatar)}
-          <br />
-          <button
-            onClick={() => setIsModalOpen(true)}
-            className="choose__avatar"
-          >
-            <span>Choose Avatar</span> <FaUserAlt className="btn-avatar" />
-          </button>
-        </div>
-        <button type="submit">Create Room</button>
-      </Form>
-      <Modal
-        open={isModalOpen}
-        styles={{
-          modal: {
-            background: constants.appAccentColor,
-            color: "white",
-          },
-        }}
-        onClose={() => handleClose()}
-        closeIcon={
-          <FaTimes
-            fill="red"
-            fontSize={"2vw"}
-            style={{
-              margin: "1vh 0",
-            }}
-          />
-        }
-      >
-        <AvatarActionBtns>
-          <h1>Choose your avatar</h1>
-        </AvatarActionBtns>
-        <AvatarsWrapper>
-          {avatars.map((avatar: string) => {
-            if (avatar !== currentAvatar) {
-              return (
-                <div onClick={() => setAvatarIcon(avatar)}>{parse(avatar)}</div>
-              );
-            } else {
-              return (
-                <div onClick={() => setAvatarIcon(avatar)} className="current">
-                  {parse(avatar)}
-                </div>
-              );
-            }
-          })}
-        </AvatarsWrapper>
-        <br />
-        <AvatarActionBtns>
-          {loading ? (
-            <AiOutlineReload className="loader" />
-          ) : (
-            <>
-              <button
-                onClick={() => {
-                  setLoading(true);
-                  setMaxAvatar((prev) => {
-                    return { isNew: false, number: prev.number + 18 };
-                  });
-                }}
-              >
-                <span>Load More</span>
-                <HiOutlineArrowDown />
-              </button>
-              <button
-                onClick={() => {
-                  setLoading(true);
-                  setMaxAvatar((prev) => {
-                    return { isNew: true, number: prev.number };
-                  });
-                }}
-              >
-                <span>Load New </span>
-                <AiOutlineReload className="loader2" />
-              </button>
-            </>
-          )}
-        </AvatarActionBtns>
-      </Modal>
-    </Page>
-  );
+	const NameRef = useRef<HTMLInputElement | null>()!;
+	const RoomRef = useRef<HTMLInputElement | null>()!;
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+	const [avatars, setAvatars] = useState<string[]>([]);
+	const [maxAvatarIndex, setMaxAvatar] = useState<maxAvatarType>({
+		number: 42,
+		isNew: true,
+	});
+	const [loading, setLoading] = useState<boolean>(false);
+	const [currentAvatar, setCurrentAvatar] = useState<string>("");
+	useEffect(() => {
+		NameRef.current?.focus();
+	}, []);
+	function handleClose() {
+		setIsModalOpen(false);
+	}
+	function setAvatarIcon(avatar: string): void {
+		handleClose();
+		setCurrentAvatar(avatar);
+	}
+	useEffect(() => {
+		if (maxAvatarIndex.isNew) {
+			let acc = [];
+			while (acc.length !== maxAvatarIndex.number) {
+				acc.push(returnRandomAvatar());
+			}
+			setAvatars(acc);
+		} else {
+			let acc = [...avatars];
+			while (acc.length !== maxAvatarIndex.number) {
+				acc.push(returnRandomAvatar());
+			}
+			setAvatars(acc);
+		}
+		setLoading(false);
+		console.log(maxAvatarIndex);
+	}, [maxAvatarIndex]);
+	function handleSubmit(e: FormEvent): void {
+		e.preventDefault();
+	}
+	useEffect(() => {
+		if (avatars.length === 42) {
+			setCurrentAvatar(avatars[0]);
+		}
+	}, [avatars]);
+	return (
+		<Page>
+			<Form onSubmit={(e) => handleSubmit(e)}>
+				<div className='field'>
+					<span>Name</span>
+					<br />
+					{/* @ts-ignore */}
+					<input type='text' ref={NameRef} spellCheck placeholder='Your Name' />
+				</div>
+				<div className='field'>
+					<span>Room Name</span>
+					<br />
+					<input
+						type='text'
+						//@ts-ignore
+						ref={RoomRef}
+						spellCheck
+						placeholder='Name Your Room'
+					/>
+				</div>
+				<div className='field'>
+					<span>Avatar</span>
+					{currentAvatar && parse(currentAvatar)}
+					<br />
+					<button
+						onClick={() => setIsModalOpen(true)}
+						className='choose__avatar'
+					>
+						<span>Choose Avatar</span> <FaUserAlt className='btn-avatar' />
+					</button>
+				</div>
+				<button type='submit'>Create Room</button>
+			</Form>
+			<Modal
+				open={isModalOpen}
+				styles={{
+					modal: {
+						background: constants.appAccentColor,
+						color: "white",
+					},
+				}}
+				onClose={() => handleClose()}
+				closeIcon={
+					<FaTimes
+						fill='red'
+						fontSize={"2vw"}
+						style={{
+							margin: "1vh 0",
+						}}
+					/>
+				}
+			>
+				<AvatarActionBtns>
+					<h1>Choose your avatar</h1>
+				</AvatarActionBtns>
+				<AvatarsWrapper>
+					{avatars.map((avatar: string) => {
+						if (avatar !== currentAvatar) {
+							return (
+								<div onClick={() => setAvatarIcon(avatar)}>{parse(avatar)}</div>
+							);
+						} else {
+							return (
+								<div onClick={() => setAvatarIcon(avatar)} className='current'>
+									{parse(avatar)}
+								</div>
+							);
+						}
+					})}
+				</AvatarsWrapper>
+				<br />
+				<AvatarActionBtns>
+					{loading ? (
+						<AiOutlineReload className='loader' />
+					) : (
+						<>
+							<button
+								onClick={() => {
+									setLoading(true);
+									setMaxAvatar((prev) => {
+										return { isNew: false, number: prev.number + 18 };
+									});
+								}}
+							>
+								<span>Load More</span>
+								<HiOutlineArrowDown />
+							</button>
+							<button
+								onClick={() => {
+									setLoading(true);
+									setMaxAvatar((prev) => {
+										return { isNew: true, number: prev.number };
+									});
+								}}
+							>
+								<span>Load New </span>
+								<AiOutlineReload className='loader2' />
+							</button>
+						</>
+					)}
+				</AvatarActionBtns>
+			</Modal>
+		</Page>
+	);
 };
 export default CreateRoom;
 function returnRandomAvatar(): string {
-  return createAvatar(style);
+	return createAvatar(style);
 }
--- src/Styled-components/CreateRoom.styled.ts
+++ src/Styled-components/CreateRoom.styled.ts
@@ -1,156 +1,168 @@
 import styled from "styled-components";
 import { constants } from "../Constants";
 export const Page = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-  height: 100vh;
-  width: 100vw;
-  background-color: ${constants.appAccentColor};
-  .btn {
-    color: white;
-  }
+	height: 100vh;
+	width: 100vw;
+	background-color: ${constants.appAccentColor};
+	.btn {
+		color: white;
+	}
 `;
 
 export const Form = styled.form`
-  background-color: #053fb3;
-  overflow-y: auto;
-  border: 1px solid black;
-  height: 36vw;
-  width: 40vw;
+	button[type="submit"] {
+		color: #053fb3;
+		background-color: yellow;
+		border: 1px solid yellow;
+		padding: 1vw 2vw;
+		&:hover {
+			color: yellow;
+			background-color: #053fb3;
+		}
+		transition: 300ms ease-ease-in-out;
+		border: 0;
+	}
+	background-color: #053fb3;
+	overflow-y: auto;
+	border: 1px solid black;
+	height: 36vw;
+	width: 40vw;
 
-  display: flex;
-  justify-content: space-around;
-  flex-direction: column;
-  align-items: center;
-  .field {
-    width: 100%;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    svg:not(.btn-avatar) {
-      width: 5vw;
-      height: 5vw;
-      border-radius: 50%;
-      border: 1px solid white;
-    }
+	display: flex;
+	justify-content: space-around;
+	flex-direction: column;
+	align-items: center;
+	.field {
+		width: 100%;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		svg:not(.btn-avatar) {
+			width: 5vw;
+			height: 5vw;
+			border-radius: 50%;
+			border: 1px solid white;
+		}
 
-    span {
-      font-size: 2vw;
-      font-family: "Poppins", sans-serif;
-      color: white;
-    }
-    input {
-      width: 80%;
-      height: 2vw;
-      font-size: 1.2vw;
-      font-family: "Open Sans", sans-serif;
-      border-radius: 5px;
-      border: 0;
-      &::placeholder {
-        font-size: 1.1vw;
-        margin-left: 5px;
-      }
-    }
-    button.choose__avatar {
-      background-color: ${constants.appAccentColor};
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-      border: 0;
-      color: white;
-      font-size: 1.1vw;
-      border-radius: 10px;
-      padding: 0 2vw;
-      height: 2.5vw;
-      transition: 300ms ease-in-out;
-      &:hover {
-        background-color: white;
-        svg,
-        span {
-          color: ${constants.appAccentColor};
-        }
-      }
-      span {
-        font-family: "Poppins", sans-serif;
-        font-size: 1.2vw;
-      }
+		span {
+			font-size: 2vw;
+			font-family: "Poppins", sans-serif;
+			color: white;
+		}
+		input {
+			width: 80%;
+			height: 2vw;
+			font-size: 1.2vw;
+			font-family: "Open Sans", sans-serif;
+			border-radius: 5px;
+			border: 0;
+			&::placeholder {
+				font-size: 1.1vw;
+				margin-left: 5px;
+			}
+		}
+		button.choose__avatar {
+			background-color: ${constants.appAccentColor};
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			justify-content: center;
+			border: 0;
+			color: white;
+			font-size: 1.1vw;
+			border-radius: 10px;
+			padding: 0 2vw;
+			height: 2.5vw;
+			transition: 300ms ease-in-out;
+			&:hover {
+				background-color: white;
+				svg,
+				span {
+					color: ${constants.appAccentColor};
+				}
+			}
+			span {
+				font-family: "Poppins", sans-serif;
+				font-size: 1.2vw;
+			}
 
-      svg {
-        padding: 0;
-        margin-left: 0.5vw;
-        color: white;
-      }
-    }
-  }
+			svg {
+				padding: 0;
+				margin-left: 0.5vw;
+				color: white;
+			}
+		}
+	}
 `;
 
 export const AvatarsWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  max-width: 40vw;
-  .current {
-    border: 2px solid yellow;
-  }
-  div {
-    border: 2px solid white;
-    svg {
-      width: 5vw;
-      height: 5vw;
-      border-radius: 50%;
+	display: flex;
+	flex-wrap: wrap;
+	max-width: 40vw;
+	.current {
+		border: 2px solid yellow;
+	}
+	div {
+		border: 2px solid white;
+		svg {
+			width: 5vw;
+			height: 5vw;
+			border-radius: 50%;
 
-      justify-content: center;
-    }
-    border-radius: 50%;
+			justify-content: center;
+		}
+		border-radius: 50%;
 
-    margin: 0.5vw;
-  }
+		margin: 0.5vw;
+	}
 `;
 
 export const AvatarActionBtns = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  .loader {
-    animation: loader 2s infinite linear;
-    font-size: 2.5vw;
-    color: white;
-    margin-bottom: 0.2vw;
-  }
-  h1 {
-    font-family: "Quicksand", sans-serif;
-    font-size: 2.5vw;
-  }
-  button {
-    margin: 1vw 1vw;
-    background-color: #1741ff;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    border: 0;
-    color: white;
-    font-size: 1.1vw;
-    border-radius: 10px;
-    padding: 0 2vw;
-    height: 2.5vw;
-    transition: 200ms ease-in-out;
-    &:hover {
-      background-color: steelblue;
-    }
-    span {
-      font-family: "Poppins", sans-serif;
-    }
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	.loader {
+		animation: loader 2s infinite linear;
+		font-size: 2.5vw;
+		color: white;
+		margin-bottom: 0.2vw;
+	}
+	h1 {
+		font-family: "Quicksand", sans-serif;
+		font-size: 2.5vw;
+	}
+	button {
+		margin: 1vw 1vw;
+		background-color: #1741ff;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		border: 0;
+		color: white;
+		font-size: 1.1vw;
+		border-radius: 10px;
+		padding: 0 2vw;
+		height: 2.5vw;
+		transition: 200ms ease-in-out;
+		&:hover {
+			background-color: steelblue;
+		}
+		span {
+			font-family: "Poppins", sans-serif;
+		}
 
-    svg {
-      padding: 0;
-      margin-left: 0.5vw;
-      color: white;
-    }
-  }
+		svg {
+			padding: 0;
+			margin-left: 0.5vw;
+			color: white;
+		}
+	}
 `;
