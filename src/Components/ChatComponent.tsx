a95df5beb44f5f1b5ddb58070c04a7ac6fbfb57a
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-08-11 14:37:08 +0530

    Added @ts-ignore so that is doesnt break on production

--- src/Components/ChatComponent.tsx
+++ src/Components/ChatComponent.tsx
@@ -106,6 +106,7 @@ const ChatComponent: FC = () => {
       Pop.play();
       //@ts-ignore
       inputRef.current.focus();
+      //@ts-ignore
       ScrollRef.current.scrollTop = ScrollRef.current.scrollHeight;
     });
   };
